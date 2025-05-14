13971aae458dd12e85d619f6efe99e393469c15d
Miguel <mgamez20@icloud.com>
2025-05-14 17:55:10 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Miguel on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesomeTests/YouAreAwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTests.swift
+//  YouAreAwesomeTests
+//
+//  Created by Miguel on 5/14/25.
+//
+
+import Testing
+@testable import YouAreAwesome
+
+struct YouAreAwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
