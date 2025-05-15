0d88e55d9f4255ecacaa3b33d162468228fc380f
Miguel <mgamez20@icloud.com>
2025-05-14 20:43:02 -0400

    Progress

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
-            Text("I am an app developer!")
-                .foregroundStyle(.red)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+//                .foregroundStyle(.red)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .fontWeight(.ultraLight)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
