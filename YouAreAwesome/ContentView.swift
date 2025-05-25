4493f93b784362a432c9a991309380f94d8fcb99
Miguel <mgamez20@icloud.com>
2025-05-24 22:44:19 -0400

    Done

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,80 +14,90 @@ struct ContentView: View {
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
     @State private var lastSoundNumber = -1
-
     @State private var audioPlayer: AVAudioPlayer!
     @State private var soundIsOn = true
     let numberOfImages = 10
     let numberOfSounds = 6
     
-    
     var body: some View {
-        VStack {
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
-                .frame(height: 100)
-                .minimumScaleFactor(0.5)
-                .animation(.easeInOut(duration: 0.15), value: message)
+        
+        ZStack {
+//            Color.orange
             
-            Spacer()
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
-                .animation(.default, value: imageName)
-            
-            
-            Spacer()
-            
-            HStack {
-                Text("Sound")
-                Toggle("", isOn: $soundIsOn)
-                    .labelsHidden()
-                    .onChange(of: soundIsOn) {
-                        if audioPlayer.isPlaying {
-                            audioPlayer.stop()
-                        }
-                    }
+            VStack {
+                Text(message)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.red)
+                    .multilineTextAlignment(.center)
+                    .minimumScaleFactor(0.5)
+                    .frame(height: 100)
+                    .animation(.easeInOut(duration: 0.15), value: message)
                 
                 Spacer()
                 
-                Button("Show Message!") {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .shadow(radius: 30)
+                    .animation(.default, value: imageName)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Sound On:")
+                    Toggle("", isOn: $soundIsOn)
+                        .labelsHidden()
+                        .onChange(of: soundIsOn) {
+                            if audioPlayer != nil && audioPlayer.isPlaying {
+                                audioPlayer.stop()
+                            }
+                        }
+
                     
-                    let messages = ["You are Awesome!", "You are Great!", "You are Amazing", "You can do it!", "When the genius bar needs hep they call you!"]
+                    Spacer()
                     
-                    
-                    
-                    lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBounds: messages.count-1)
-//                    message = messages[lastMessageNumber]
-                    
-                    lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBounds: numberOfImages-1)
-                    imageName = "image\(lastImageNumber)"
-                    
-                    lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBounds: numberOfSounds-1)
-                    if soundIsOn {
-                        playSound(soundName: "sound\(lastSoundNumber)")
+                    Button("Show Message!") {
+                        let messages = ["You are Awesome!",
+                                        "When the Genius Bar needs Help, They Call You!",
+                                        "You Are Great!",
+                                        "You Are Fantastic!",
+                                        "Fabulous? That's You!",
+                                        "You Make Me Smile!"]
+                        
+                        lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBounds: messages.count-1)
+                        print($lastMessageNumber)
+                        //                    message = messages[lastMessageNumber]
+                        
+                        lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBounds: numberOfImages-1)
+                        imageName = "image\(lastImageNumber)"
+                        
+                        lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBounds: numberOfSounds-1)
+                        if soundIsOn {
+                            playSound(soundName: "sound\(lastSoundNumber)")
+                        }
                     }
+                    .buttonStyle(.borderedProminent)
+                    .font(.title2)
                 }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
             }
+            .tint(.accentColor)
+            .padding()
         }
-        .padding()
     }
-    func nonRepeatingRandom (lastNumber: Int, upperBounds: Int) -> Int {
+    func nonRepeatingRandom(lastNumber: Int, upperBounds: Int) -> Int {
            var newNumber : Int
            repeat {
                newNumber = Int.random(in: 0...upperBounds)
            } while newNumber == lastNumber
-           return lastNumber
+        return newNumber
        }
     
     func playSound (soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+                audioPlayer.stop()
+        }
         guard let soundFile = NSDataAsset(name: soundName) else {
             print("😡 Could not read file name")
             return
@@ -104,8 +114,13 @@ struct ContentView: View {
    
 }
 
-#Preview {
+#Preview ("Light Mode"){
     ContentView()
+        .preferredColorScheme(.light)
 }
 
 
+#Preview ("Dark Mode"){
+    ContentView()
+        .preferredColorScheme(.dark)
+}
